1916fce03a3234f500699c31bb72c06d38043c63
benkrakar <benkrakarwalid44@gmail.com>
2023-05-04 14:38:38 +0100

    feat: adding main layout components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+      },
       fontFamily: {
         'sans': ['Sen', ...defaultTheme.fontFamily.sans],
       },
