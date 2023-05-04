ad1b6111aa5e0146cce80948c37d378ef65fff42
benkrakar <benkrakarwalid44@gmail.com>
2023-05-04 12:19:52 +0100

    feat: adding vite plugins pages and layouts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), Pages(), Layouts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
