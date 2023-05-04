4aa9ca451eafc44cdb6c8fb7f01a033a19f4f4d9
Benkrakar Walid <benkrakarwalid44@gmail.com>
2023-05-04 14:40:38 +0100

    feat: adding main layout components (#5)

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+</script>
 <template>
-  <div>
+  <div class="bg-gray-100 min-h-screen ">
+    <Navbar />
     <RouterView />
   </div>
 </template>
